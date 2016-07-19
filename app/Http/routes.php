b877fd3e11d270504e56dc52429ceb4bfb784ec2
Zach Preston <zpdrums@comcast.net>
2016-07-18 22:23:29 -0400

    fix index route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,9 @@
 <?php
 
+//Welcome route
+Route::get('/', 'WelcomeController@index');
+
 //Authentication routes
-Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
