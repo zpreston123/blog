d29e52cde0699b601e1fadba8f1f0f671891b398
Zach Preston <zpdrums@comcast.net>
2016-07-31 17:28:32 -0400

    clean up routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,24 +4,17 @@
 Route::get('/', 'WelcomeController@index');
 
 //Authentication routes
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::auth();
 
-//Registration routes
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-//Password reset link request routes
-Route::get('password/email', 'Auth\PasswordController@getEmail');
-Route::post('password/email', 'Auth\PasswordController@postEmail');
-
-//Password reset routes
-Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-Route::post('password/reset', 'Auth\PasswordController@postReset');
-
+//Home route
 Route::get('home', ['as' => 'home', 'uses' => 'PostController@index']);
+
+//Profile routes
 Route::get('profile', 'UserController@profile');
 Route::post('profile', 'UserController@updateAvatar');
 
+//Post routes
 Route::resource('posts', 'PostController');
+
+//User routes
 Route::resource('users', 'UserController');
