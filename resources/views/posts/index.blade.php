1bee2b6a40ae60cb722fa438d8cd643656e70fe9
Zach Preston <zpdrums@comcast.net>
2016-08-10 11:41:14 -0400

    clean up posts index view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,41 +4,31 @@
 
 @section('content')
     @if ($posts->count() > 0)
-        <div class="row">
-            <div class="col l12">
-                @foreach ($posts as $post)
-                    <div class="row">
-                        <div class="col l12 col m9">
-                            <h3><a href="{{ url('posts/'.$post->id) }}">{{ $post->title }}</a></h3>
-                            <div class="row">
-                                <div class="col l8 col s12">
-                                    <p>{{ mb_strimwidth(strip_tags($post->body), 0, 100, "...") }}</p>
-                                    <p>
-                                        <a href="{{ url('posts/'.$post->id) }}" class="waves-effect waves-light btn">Read More</a>
-                                        @if ($post->user->id === auth()->id())
-                                            <a href="{{ url('posts/'.$post->id.'/edit') }}" class="waves-effect waves-light btn orange">Edit</a>
-                                            {{ Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) }}
-                                                <button type="submit" class="waves-effect waves-light btn red deletePost">Delete</button>
-                                            {{ Form::close() }}
-                                        @endif
-                                    <ul>
-                                        <li><i class="fa fa-user" aria-hidden="true"></i> {{ $post->user->name }}</li>
-                                        <li><i class="fa fa-calendar" aria-hidden="true"></i> Published {{ $post->created_at->diffForHumans() }}</li>
-                                        <li>
-                                            <a class="commentsLink" href="#comments"><i class="fa fa-comment"></i> {{ $post->comments()->count() }} Comments</a>
-                                        </li>
-                                    </ul>
-                                    {{-- @include('comments.index', ['post' => $post]) --}}
-                                </div>
-                                <div class="col s3"></div>
-                            </div><br><br>
-                        </div>
-                    </div>
-                    <hr>
-                @endforeach
-                {{ $posts->render() }}
+        @foreach ($posts as $post)
+            <div class="row">
+                <div class="col s12">
+                    <h4><a href="{{ url('posts/'.$post->id) }}">{{ $post->title }}</a></h4>
+                </div>
+                <div class="col s12">
+                    <p class="truncate">{{ strip_tags($post->body) }}</p>
+                    <a href="{{ url('posts/'.$post->id) }}" class="waves-effect waves-light btn">Read More</a>
+                    @if ($post->user->id === auth()->id())
+                        <a href="{{ url('posts/'.$post->id.'/edit') }}" class="waves-effect waves-light btn orange">Edit</a>
+                        {{ Form::open(['route' => ['posts.destroy', $post->id], 'style' => 'display:inline;', 'method' => 'delete']) }}
+                            <button type="submit" class="waves-effect waves-light btn red deletePost">Delete</button>
+                        {{ Form::close() }}
+                    @endif
+                </div>
+                <div class="col s12">
+                    <ul>
+                        <li style="display:inline;"><i class="fa fa-user" aria-hidden="true"></i> {{ $post->user->name }}</li>&nbsp;|&nbsp;
+                        <li style="display:inline;"><i class="fa fa-calendar" aria-hidden="true"></i> Published {{ $post->created_at->diffForHumans() }}</li>
+                    </ul>
+                </div>
             </div>
-        </div>
+            <div class="divider"></div>
+        @endforeach
+        {{ $posts->render() }}
     @else
         <p>No posts have been made!  Please check back later.</p>
     @endif
