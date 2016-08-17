8da39092e1ccac595a9e056c9c54cc3b189f1afb
Zach Preston <zpdrums@comcast.net>
2016-08-17 12:23:34 -0400

    fix navbar

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,39 +1,38 @@
-<!-- Dropdown Structure -->
 <ul id="dropdown1" class="dropdown-content">
-  <li><a href="{{ url('profile') }}"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
-  <li><a href="{{ url('logout') }}"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+  <li><a href="{{ url('profile') }}">Profile</a></li>
+  <li><a href="{{ url('logout') }}">Logout</a></li>
 </ul>
 
 <nav class="light-blue lighten-1">
   <div class="nav-wrapper">
     <a class="brand-logo" href="{{ (auth()->check()) ? "/posts" : "/" }}">EXPress Blog</a>
-    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      @if (auth()->check())
-        <ul class="right hide-on-med-and-down">
-          <li><a href="{{ url('posts/create') }}">New Post</a></li>
-          <li>
-            <form action="/users/search" method="GET">
-              <div class="input-field">
-                <input id="query" name="query" type="search">
-                <label for="search"><i class="material-icons">search</i></label>
-                <i class="material-icons">close</i>
-              </div>
-            </form>
-          </li>
-          <!-- Dropdown Trigger -->
-          <li>
-            <a class="dropdown-button" href="#!" data-activates="dropdown1" style="position:relative; padding-left:50px;">
-              <img src="/uploads/avatars/{{ auth()->user()->avatar }}" style="width:32px; height:32px; position:absolute; top:15px; left:10px; border-radius:50%;">
-              {{ auth()->user()->name }}<i class="material-icons right">arrow_drop_down</i>
-            </a>
-          </li>
-        </ul>
-       <ul class="side-nav" id="mobile-demo">
+
+    @if (auth()->check())
+      <ul class="right hide-on-med-and-down">
+        <li><a href="{{ url('posts/create') }}">New Post</a></li>
+        <li>
+          <form action="/users/search" method="GET">
+            <div class="input-field">
+              <input id="query" name="query" type="search">
+              <label for="search"><i class="material-icons">search</i></label>
+              <i class="material-icons">close</i>
+            </div>
+          </form>
+        </li>
+        <li>
+          <a class="dropdown-button" href="#!" data-activates="dropdown1" style="position:relative; padding-left:50px;">
+            <img src="/uploads/avatars/{{ auth()->user()->avatar }}" style="width:32px; height:32px; position:absolute; top:15px; left:10px; border-radius:50%;">
+            {{ auth()->user()->name }}<i class="material-icons right">arrow_drop_down</i>
+          </a>
+        </li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
         <li><a href="collapsible.html">Javascript</a></li>
         <li><a href="mobile.html">Mobile</a></li>
       </ul>
-      @endif
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+    @endif
   </div>
 </nav>
