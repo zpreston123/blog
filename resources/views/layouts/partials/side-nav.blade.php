cee85aec9e6b9f426e84fb4272fbecb6a62a358b
Zach Preston <zpdrums@comcast.net>
2016-08-20 15:39:22 -0400

    update side nav for mobile

--- resources/views/layouts/partials/side-nav.blade.php
+++ resources/views/layouts/partials/side-nav.blade.php
@@ -1,6 +1,9 @@
 <ul class="side-nav" id="mobile-demo">
-	<li><a href="sass.html">Sass</a></li>
-	<li><a href="badges.html">Components</a></li>
-	<li><a href="collapsible.html">Javascript</a></li>
-	<li><a href="mobile.html">Mobile</a></li>
+	<li><a href="{{ url('posts/create') }}">New Post</a></li>
+	<li>
+	  <a class="dropdown-button" href="#!" data-activates="dropdown1" style="position:relative; padding-left:50px;">
+	    <img src="/uploads/avatars/{{ auth()->user()->avatar }}" style="width:32px; height:32px; position:absolute; top:15px; left:10px; border-radius:50%;">
+	    {{ auth()->user()->name }}<i class="material-icons right">arrow_drop_down</i>
+	  </a>
+	</li>
 </ul>
