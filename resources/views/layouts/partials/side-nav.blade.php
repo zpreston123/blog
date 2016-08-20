d11c6ff0a01516923f6b0159e64d35c6d7c149f0
Zach Preston <zpdrums@comcast.net>
2016-08-20 15:49:38 -0400

    update sidebar again for mobile

--- resources/views/layouts/partials/side-nav.blade.php
+++ resources/views/layouts/partials/side-nav.blade.php
@@ -1,7 +1,12 @@
+<ul id="dropdown2" class="dropdown-content">
+  <li><a href="{{ url('profile') }}">Profile</a></li>
+  <li><a href="{{ url('logout') }}">Logout</a></li>
+</ul>
+
 <ul class="side-nav" id="mobile-demo">
 	<li><a href="{{ url('posts/create') }}">New Post</a></li>
 	<li>
-	  <a class="dropdown-button" href="#!" data-activates="dropdown1" style="position:relative; padding-left:50px;">
+	  <a class="dropdown-button" href="#!" data-activates="dropdown2" style="position:relative; padding-left:50px;">
 	    <img src="/uploads/avatars/{{ auth()->user()->avatar }}" style="width:32px; height:32px; position:absolute; top:15px; left:10px; border-radius:50%;">
 	    {{ auth()->user()->name }}<i class="material-icons right">arrow_drop_down</i>
 	  </a>
