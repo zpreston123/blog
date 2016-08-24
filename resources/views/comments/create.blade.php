ae0c63db7f1cf5b2ba116edc864abe8ca566057b
Zach Preston <zpdrums@comcast.net>
2016-08-24 18:17:57 -0400

    update comment views - needs to be fixed

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -2,7 +2,7 @@
 	<legend>Add a Comment:</legend>
 	<div class="input-field">
 		<div class="form-group">
-			{!! Form::text('body', ['class' => 'form-control', 'placeholder' => 'Body']) !!}
+			{!! Form::text('body', null, ['class' => 'form-control', 'placeholder' => 'Body']) !!}
 		</div>
 	</div>
 	{!! Form::submit('Submit', ['class' => 'btn']) !!}
--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -1,13 +1,15 @@
 <ul class="collection">
 	@foreach ($post->comments as $comment)
 	    <li class="collection-item avatar">
-	     	<img src="#" alt="" class="circle">
+	     	<img src="/uploads/avatars/{{ $comment->user->avatar }}" alt="{{ $comment->user->name }}" class="circle">
 	     	<span class="title">{{ $comment->user->name }}</span>
 			<p>
 				{{ $comment->body }}<br>
 				{{ $comment->created_at->diffForHumans() }}
 			</p>
-	     	<a href="#!" class="secondary-content"><i class="material-icons">delete</i></a>
+			@if ((auth()->id() == $comment->user->id) || ($post->user->id == auth()->id()))
+		     	<a href="#!" class="secondary-content"><i class="material-icons">delete</i></a>
+	     	@endif
 	    </li>
 	@endforeach
 </ul>
