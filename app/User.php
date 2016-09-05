fdfd1714d1c74478522f3af482dcb1ff7b12b3e0
Zach Preston <zpdrums@comcast.net>
2016-09-05 15:28:59 -0400

    update post and user models

--- app/User.php
+++ app/User.php
@@ -10,13 +10,6 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array
