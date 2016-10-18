e1fdcf39e047d47377dd312034ca89246a3e8a27
Zach Preston <zpdrums@comcast.net>
2016-10-17 23:30:47 -0400

    add icon to new post link in the navbars

--- resources/views/layouts/partials/side-nav.blade.php
+++ resources/views/layouts/partials/side-nav.blade.php
@@ -8,10 +8,10 @@
 </ul>
 
 <ul class="side-nav" id="mobile-demo">
-	<li><a href="{{ url('posts/create') }}">New Post</a></li>
+	<li><a href="{{ url('posts/create') }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> New Post</a></li>
 	<li>
 	  <a class="dropdown-button" href="#!" data-activates="dropdown2" style="position:relative; padding-left:50px;">
-	    <img src="/uploads/avatars/{{ auth()->user()->avatar }}" style="width:32px; height:32px; position:absolute; top:15px; left:10px; border-radius:50%;">
+	    <img src="{{ auth()->user()->avatar }}" style="width:32px; height:32px; position:absolute; top:15px; left:10px; border-radius:50%;">
 	    {{ auth()->user()->name }}<i class="material-icons right">arrow_drop_down</i>
 	  </a>
 	</li>
