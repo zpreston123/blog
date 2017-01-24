3e4b11177dbf556403b166b381f587c65e7b851e
Zach Preston <zpdrums@comcast.net>
2017-01-24 18:07:22 -0500

    add icons to login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,18 @@
 {!! Form::open(['url' => 'login']) !!}
     {!! Form::label('email', 'Email', ['class' => 'label']) !!}
-    <p class="control">
+    <p class="control has-icon">
         {!! Form::text('email', old('email'), ['class' => 'input', 'placeholder' => 'jdoe@example.com']) !!}
+        <span class="icon is-small">
+            <i class="fa fa-envelope"></i>
+        </span>
         {!! $errors->first('email', '<span class="help is-danger">:message</span>') !!}
     </p>
     {!! Form::label('password', 'Password', ['class' => 'label']) !!}
-    <p class="control">
+    <p class="control has-icon">
         {!! Form::password('password', ['class' => 'input', 'placeholder' => '*******']) !!}
+        <span class="icon is-small">
+            <i class="fa fa-lock"></i>
+        </span>
         {!! $errors->first('password', '<span class="help is-danger">:message</span>') !!}
     </p>
     <p class="control">
