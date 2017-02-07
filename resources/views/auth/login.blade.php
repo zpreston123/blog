75124590a415f3f8044038a4c8a49eb906879ffa
Zach Preston <zpdrums@comcast.net>
2017-02-07 15:26:07 -0500

    restyled welcome page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,23 @@
-{!! Form::open(['url' => 'login']) !!}
-    {!! Form::label('email', 'Email', ['class' => 'label']) !!}
-    <p class="control has-icon">
-        {!! Form::text('email', old('email'), ['class' => 'input', 'placeholder' => 'jdoe@example.com']) !!}
-        <span class="icon is-small">
-            <i class="fa fa-envelope"></i>
-        </span>
-        {!! $errors->first('email', '<span class="help is-danger">:message</span>') !!}
-    </p>
-    {!! Form::label('password', 'Password', ['class' => 'label']) !!}
-    <p class="control has-icon">
-        {!! Form::password('password', ['class' => 'input', 'placeholder' => '*******']) !!}
-        <span class="icon is-small">
-            <i class="fa fa-lock"></i>
-        </span>
-        {!! $errors->first('password', '<span class="help is-danger">:message</span>') !!}
-    </p>
-    <p class="control">
-        {!! Form::submit('Log in', ['class' => 'button is-primary']) !!}
-    </p>
-{!! Form::close() !!}
+<div class="box">
+    {!! Form::open(['url' => 'login']) !!}
+        {!! Form::label('email', 'Email', ['class' => 'label']) !!}
+        <p class="control has-icon">
+            {!! Form::text('email', old('email'), ['class' => 'input', 'placeholder' => 'jdoe@example.com']) !!}
+            <span class="icon is-small">
+                <i class="fa fa-envelope"></i>
+            </span>
+            {!! $errors->first('email', '<span class="help is-danger">:message</span>') !!}
+        </p>
+        {!! Form::label('password', 'Password', ['class' => 'label']) !!}
+        <p class="control has-icon">
+            {!! Form::password('password', ['class' => 'input', 'placeholder' => '*******']) !!}
+            <span class="icon is-small">
+                <i class="fa fa-lock"></i>
+            </span>
+            {!! $errors->first('password', '<span class="help is-danger">:message</span>') !!}
+        </p>
+        <p class="control">
+            {!! Form::submit('Log in', ['class' => 'button is-primary']) !!}
+        </p>
+    {!! Form::close() !!}
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,16 @@
 @section('title', 'Welcome')
 
 @section('content')
-    <div class="row" style="margin-top: 20px;">
-        <div class="col l7">
+    <div class="columns">
+        <div class="column is-three-quarters">
             <h1>Welcome!</h1>
             <p>Express yourself any time and share it with your family and friends.</p>
             <p>Please log in or click below to register for an account:</p>
             <p>
-                <a class="waves-effect waves-light btn btn-large" href="{{ url('register') }}" role="button">Sign Up</a>
+                <a class="button is-medium is-primary" href="{{ url('register') }}">Sign Up</a>
             </p>
         </div>
-        <div class="col l5">
+        <div class="column">
             @include('auth.login')
         </div>
     </div>
