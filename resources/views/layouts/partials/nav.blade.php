1131e626e25e559600b08de61a931d02d1d11522
Zach Preston <zpdrums@comcast.net>
2017-06-12 15:23:03 -0400

    front end tweaks to posts page and sass refactoring

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -3,8 +3,8 @@
     <div class="nav-left">
       <a href="{{ (auth()->check()) ? "/posts" : "/" }}" class="nav-item">EXPress Blog</a>
       @if (auth()->check())
-        <a href="{{ url('posts/create') }}" class="nav-item is-tab {{ Request::is('posts/create') ? 'is-active' : '' }}">New Post</a>
-        <a class="nav-item is-tab {{ Request::is('my_favorites') ? 'is-active' : '' }}" href="{{ url('my_favorites') }}">My Favorites</a>
+        <a href="{{ url('posts/create') }}" class="nav-item {{ Request::is('posts/create') ? 'is-active' : '' }}">New Post</a>
+        <a class="nav-item {{ Request::is('my_favorites') ? 'is-active' : '' }}" href="{{ url('my_favorites') }}">My Favorites</a>
       @endif
     </div>
     <span class="nav-toggle">
@@ -14,13 +14,13 @@
     </span>
     @if (auth()->check())
       <div class="nav-right nav-menu">
-        <a class="nav-item is-tab" href="{{ url('profile/'.auth()->id()) }}">
+        <a class="nav-item" href="{{ url('profile/'.auth()->id()) }}">
           <figure class="image is-16x16" style="margin-right: 8px;">
             <img src="{{ auth()->user()->avatar }}">
           </figure>
           {{ auth()->user()->name }}
         </a>
-        <a href="{{ url('logout') }}" class="nav-item is-tab" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a>
+        <a href="{{ url('logout') }}" class="nav-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a>
         {!! Form::open(['id' => 'logout-form', 'url' => 'logout']) !!}
         {!! Form::close() !!}
       </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,38 +4,49 @@
 
 @section('content')
     <div class="row">
-        {!! Form::open(['url' => 'posts/search', 'style' => 'padding-bottom:40px;', 'method' => 'GET']) !!}
-            {!! Form::text('q', old('q'), ['type' => 'search', 'class' => 'search', 'placeholder' => 'Search...']) !!}
-        {!! Form::close() !!}
+        <div class="columns">
+            <div class="column is-10 is-offset-1">
+                {!! Form::open(['url' => 'posts/search', 'style' => 'padding-bottom:40px;', 'method' => 'GET']) !!}
+                    {!! Form::text('q', old('q'), ['type' => 'search', 'class' => 'search', 'placeholder' => 'Search...']) !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
     </div>
     <div class="row">
-        @if ($posts->count() > 0)
-            @foreach ($posts as $post)
-                <div class="box">
-                    <h2>{{ $post->title }}</h2>
-                    <p>{{ str_limit($post->body, 200) }}</p>
-                    @unless($post->tags->isEmpty())
-                        <ul class="tags" style="list-style-type: none; margin: 0;">
-                            @foreach ($post->tags as $tag)
-                               <li style="display: inline-block;">
-                                    <span class="tag is-info is-medium">
-                                        {{ $tag->name }}
-                                    </span>
-                                </li>
-                            @endforeach
-                        </ul><br>
-                    @endunless
-                    <p>{{ $post->author->name }}&nbsp;|&nbsp;Published {{ $post->created_at->diffForHumans() }}</p>
-                    <a href="{{ url('posts/'.$post->id) }}" class="button is-primary">Read More</a><br><br>
-                    <favorite
-                        :post={{ $post->id }}
-                        :favorited={{ $post->favorited() ? 'true' : 'false' }}
-                    ></favorite>
-                </div>
-            @endforeach
-            {{ $posts->links() }}
-        @else
-            <p>No posts have been made!  Please check back later.</p>
-        @endif
+        <div class="columns">
+            <div class="column is-10 is-offset-1">
+                @if ($posts->count() > 0)
+                    @foreach ($posts as $post)
+                        <div class="box">
+                            <h2>
+                                <a class="post-title" href="{{ route('posts.show', $post->id) }}">
+                                    {{ $post->title }}
+                                </a>
+                            </h2>
+                            <p>{{ str_limit($post->body, 200) }}</p>
+                            @unless($post->tags->isEmpty())
+                                <ul class="tags" style="list-style-type: none; margin: 0;">
+                                    @foreach ($post->tags as $tag)
+                                       <li style="display: inline-block;">
+                                            <span class="tag is-info is-medium">
+                                                {{ $tag->name }}
+                                            </span>
+                                        </li>
+                                    @endforeach
+                                </ul><br>
+                            @endunless
+                            <p>{{ $post->author->name }}&nbsp;|&nbsp;Published {{ $post->created_at->diffForHumans() }}</p>
+                            <favorite
+                                :post={{ $post->id }}
+                                :favorited={{ $post->favorited() ? 'true' : 'false' }}
+                            ></favorite>
+                        </div>
+                    @endforeach
+                    {{ $posts->links() }}
+                @else
+                    <p>No posts have been made!  Please check back later.</p>
+                @endif
+            </div>
+        </div>
     </div>
 @endsection
