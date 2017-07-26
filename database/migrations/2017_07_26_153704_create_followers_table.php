0be7f83df9ff2a2f3c5d06027cb829787655ed63
Zach Preston <zpdrums@comcast.net>
2017-07-26 12:56:53 -0400

    add followers table migration

---
+++ database/migrations/2017_07_26_153704_create_followers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('follower_id')->unsigned()->index();
+            $table->integer('followee_id')->unsigned()->index();
+            $table->timestamp('created_at');
+
+            $table->foreign('follower_id')->references('id')->on('users');
+            $table->foreign('followee_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('followers');
+    }
+}
