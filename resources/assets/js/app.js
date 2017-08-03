b00da3575478e45ef93647889d5d50a2ec5efbc0
Zach Preston <zpdrums@comcast.net>
2017-08-02 21:46:44 -0400

    remove example vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,7 +14,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
 Vue.component('comments', require('./components/Comments.vue'));
 Vue.component('favorite', require('./components/Favorite.vue'));
 
