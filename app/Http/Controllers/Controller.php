88bb023faff49baa75220a1c9d48b166b61457fa
Zach Preston <zpdrums@comcast.net>
2017-08-03 15:00:22 -0400

    remove unnecessary base controller variables

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,21 +2,11 @@
 
 namespace Blog\Http\Controllers;
 
-use Blog\User;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller extends BaseController
 {
-    use DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Constructor.
-     */
-    public function __construct()
-    {
-        view()->share('user', auth()->user());
-        view()->share('signedIn', auth()->check());
-    }
+	use DispatchesJobs, ValidatesRequests;
 }
