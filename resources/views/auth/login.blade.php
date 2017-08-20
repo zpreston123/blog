b800487230e903439c7fc76aaf5264bb0b5b7fc7
Zach Preston <zpdrums@comcast.net>
2017-08-20 19:37:10 -0400

    rewrite auth views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,30 +2,34 @@
     <form method="POST" action="/login">
         {{ csrf_field() }}
 
-        <label for="email" class="label">Email</label>
-        <p class="control has-icon">
-            <input type="text" name="email" value="{{ old('email') }}" class="input">
-            <span class="icon is-small">
-                <i class="fa fa-envelope"></i>
-            </span>
-            <span class="help is-danger">
+        <div class="field">
+            <label class="label">Email</label>
+            <div class="control has-icons-left">
+                <input class="input" type="text" name="email" value="{{ old('email') }}">
+                <span class="icon is-small is-left">
+                    <i class="fa fa-envelope"></i>
+                </span>
+            </div>
+            <p class="help is-danger">
                 {{ $errors->first('email') }}
-            </span>
-        </p>
+            </p>
+        </div>
 
-        <label for="password" class="label">Password</label>
-        <p class="control has-icon">
-            <input type="password" name="password" class="input">
-            <span class="icon is-small">
-                <i class="fa fa-lock"></i>
-            </span>
-            <span class="help is-danger">
+        <div class="field">
+            <label class="label">Password</label>
+            <div class="control has-icons-left">
+                <input class="input" type="password" name="password">
+                <span class="icon is-small is-left">
+                    <i class="fa fa-lock"></i>
+                </span>
+            </div>
+            <p class="help is-danger">
                 {{ $errors->first('password') }}
-            </span>
-        </p>
+            </p>
+        </div>
 
-        <p class="control">
-            <input type="submit" value="Log in" class="button is-primary">
-        </p>
+        <div class="control">
+            <button class="button is-primary" type="submit">Log in</button>
+        </div>
     </form>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,39 +8,68 @@
             <h1 class="title has-text-centered">Register an Account</h1>
 
             <div class="box">
-                <form action="/register" method="POST">
+                <form method="POST" action="/register">
                     {{ csrf_field() }}
 
-                    <label for="name" class="label">Name</label>
-                    <p class="control">
-                        <input type="text" name="name" value="{{ old('name') }}" class="input">
-                        <span class="help is-danger">{{ $errors->first('name') }}</span>
-                    </p>
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="text" name="name" value="{{ old('name') }}">
+                            <span class="icon is-small is-left">
+                                <i class="fa fa-user"></i>
+                            </span>
+                        </div>
+                        <p class="help is-danger">
+                            {{ $errors->first('name') }}
+                        </p>
+                    </div>
 
-                    <label for="email" class="label">Email</label>
-                    <p class="control">
-                        <input type="text" name="email" value="{{ old('email') }}" class="input">
-                        <span class="help is-danger">{{ $errors->first('email') }}</span>
-                    </p>
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="text" name="email" value="{{ old('email') }}">
+                            <span class="icon is-small is-left">
+                                <i class="fa fa-envelope"></i>
+                            </span>
+                        </div>
+                        <p class="help is-danger">
+                            {{ $errors->first('email') }}
+                        </p>
+                    </div>
 
                     <hr>
 
-                    <label for="password" class="label">Password</label>
-                    <p class="control">
-                        <input type="password" name="password" class="input">
-                        <span class="help is-danger">{{ $errors->first('password') }}</span>
-                    </p>
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="password" name="password">
+                            <span class="icon is-small is-left">
+                                <i class="fa fa-lock"></i>
+                            </span>
+                        </div>
+                        <p class="help is-danger">
+                            {{ $errors->first('password') }}
+                        </p>
+                    </div>
 
-                    <label for="password_confirmation" class="label">Confirm Password</label>
-                    <p class="control">
-                        <input type="password" name="password_confirmation" class="input">
-                        <span class="help is-danger">{{ $errors->first('password_confirmation') }}</span>
-                    </p>
+                    <div class="field">
+                        <label class="label">Confirm Password</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="password" name="password_confirmation">
+                             <span class="icon is-small is-left">
+                                <i class="fa fa-check-circle"></i>
+                            </span>
+                        </div>
+                    </div>
 
-                    <p class="control">
-                        <input type="submit" value="Register" class="button is-primary">
-                        <input type="button" value="Cancel" onclick="document.location.href='/'" class="button is-danger">
-                    </p>
+                    <div class="field is-grouped">
+                        <p class="control">
+                            <button class="button is-primary" type="submit">Register</button>
+                        </p>
+                        <p class="control">
+                            <button class="button is-danger" onclick="document.location.href='/'">Cancel</button>
+                        </p>
+                    </div>
                 </form>
             </div>
         </div>
