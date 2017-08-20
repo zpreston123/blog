945c437001c687d75c5639610cb9d404e43aa25d
Zach Preston <zpdrums@comcast.net>
2017-08-20 16:52:32 -0400

    update master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>@yield('title')</title>
 
     <link rel="stylesheet" href="/css/app.css">
-
-    <script>
-        window.Laravel = { 'csrfToken': '{{ csrf_token() }}' };
-    </script>
 </head>
 <body>
-    @include('layouts.partials.nav')
+    @include('layouts.partials.navbar')
 
     <section class="section">
         <div id="app" class="container content">
@@ -26,7 +24,6 @@
     @include('layouts.partials.footer')
 
     <script src="/js/app.js"></script>
-
     @yield('scripts')
 </body>
 </html>
