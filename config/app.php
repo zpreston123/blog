07362a1295e6745e9f571e8307a44becbbf94918
Zach Preston <zpdrums@comcast.net>
2017-09-11 22:59:04 -0400

    remove extra package service providers and aliases, since they're automatically loaded

--- config/app.php
+++ config/app.php
@@ -167,8 +167,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Collective\Html\HtmlServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,9 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+
     ],
 
 ];
