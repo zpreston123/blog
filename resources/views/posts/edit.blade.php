733786f5081ad135008324c395749ac3a8cadef9
Zach Preston <zpdrums@comcast.net>
2017-09-14 23:24:38 -0400

    change http method in edit post form to patch

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,7 +8,7 @@
     <div class="columns">
         <div class="column is-half is-offset-one-quarter">
             <div class="box">
-                {{ Form::model($post, ['route' => ['posts.update', $post], 'method' => 'PUT']) }}
+                {{ Form::model($post, ['route' => ['posts.update', $post], 'method' => 'PATCH']) }}
                     @include('posts.form', ['submitButtonText' => 'Update'])
                 {{ Form::close() }}
             </div>
