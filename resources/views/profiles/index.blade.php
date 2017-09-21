fcfe16d0d4ff975a9603961922440f9751b9e622
Zach Preston <zpdrums@comcast.net>
2017-09-21 15:26:57 -0400

    rebuild user search results page

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,15 +3,27 @@
 @section('title', 'Users')
 
 @section('content')
+	<h1 class="title">All Results ({{ $users->count() }})</h1>
+                <hr>
+
+	<div class="box">
 	@foreach ($users as $user)
-		<ul>
-			<li><img src="{{ $user->avatar }}"></li>
-			<li>Name:
-				<a href="/profiles/{{ $user->id }}">
-					{{ $user->name }}
-				</a>
-			</li>
-			<li>Email: {{ $user->email }}</li>
-		</ul>
+		<article class="media">
+			<div class="media-left">
+				<figure class="image is-64x64">
+					<img src="{{ $user->avatar }}">
+				</figure>
+			</div>
+			<div class="media-content">
+				<div class="content">
+					<p>
+						<a href="profiles/{{ $user->id }}">
+							<strong>{{ $user->name }}</strong>
+						</a>
+					</p>
+				</div>
+			</div>
+		</article>
 	@endforeach
+	</div>
 @endsection
