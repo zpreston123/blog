ca1cd651a8f6336cd73c2d9eed3def4c7b1d9e8e
Zach Preston <zpdrums@comcast.net>
2017-10-16 17:55:30 -0400

    add made with bulma badge to footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,6 +4,9 @@
 			<p>
 				<strong>Blog Xpress</strong> by Zach Preston.
 			</p>
+			<a href="https://bulma.io">
+				<img src="/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
+			</a>
 		</div>
 	</div>
 </footer>
