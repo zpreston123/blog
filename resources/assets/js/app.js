306116cb6b5bccd2d73dc8bc44697588efd3a3a6
Zach Preston <zpdrums@comcast.net>
2017-10-16 23:24:45 -0400

    fix toggle burger function bug

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -26,12 +26,23 @@ const app = new Vue({
 	el: '#app'
 });
 
-const navbarBurger = document.querySelector('.navbar-burger');
+document.addEventListener('DOMContentLoaded', () => {
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-navbarBurger.addEventListener('click', () => {
-	const target = this.dataset.target;
-	const $target = document.getElementById(target);
+	// Check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+		// Add a click event on each of them
+		$navbarBurgers.forEach($el => {
+			$el.addEventListener('click', () => {
+				// Get the target from the "data-target" attribute
+				const target = $el.dataset.target;
+				const $target = document.getElementById(target);
 
-	this.classList.toggle('is-active');
-	$target.classList.toggle('is-active');
+				// Toggle the class on both the "navbar-burger" and the "navbar-menu"
+				$el.classList.toggle('is-active');
+				$target.classList.toggle('is-active');
+			});
+		});
+	}
 });
