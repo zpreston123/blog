e1dca24619e6e9d27249f064b073cf67fd18a965
Zach Preston <zpdrums@comcast.net>
2017-11-29 09:42:19 -0500

    reformat master file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,33 +1,26 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('title')</title>
-
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    <div id="app">
-        @include('layouts.partials.navbar')
-
-        <section class="section">
-            <div class="container content">
-                @include('flash::message')
-
-                @yield('content')
-
-                <flash-message></flash-message>
-            </div>
-        </section>
-
-        @include('layouts.partials.footer')
-    </div>
-
-    <script src="/js/app.js"></script>
-    @yield('scripts')
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            @include('layouts.partials.navbar')
+            <section class="section">
+                <div class="container content">
+                    @include('flash::message')
+                    @yield('content')
+                    <flash-message></flash-message>
+                </div>
+            </section>
+            @include('layouts.partials.footer')
+        </div>
+        <script src="/js/app.js"></script>
+        @yield('scripts')
+    </body>
 </html>
