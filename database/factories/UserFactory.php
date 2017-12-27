44cdacade5c7a3b60e48dc8db2f2e9c40c0edb26
Zach Preston <zpdrums@comcast.net>
2017-12-26 23:10:00 -0500

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,16 +14,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(Blog\User::class, function (Faker $faker) {
-    static $password;
+	$gender = $faker->randomElement(['male', 'female']);
 
-    $gender = $faker->randomElement(['male', 'female']);
-
-    return [
-        'name' => $faker->name($gender),
-        'email' => $faker->unique()->safeEmail,
-        'gender' => $gender,
-        'avatar' => 'default-'.$gender.'.jpg',
-        'password' => $password ?: $password = 'secret',
-        'remember_token' => str_random(10),
-    ];
+	return [
+		'name' => $faker->name($gender),
+		'email' => $faker->unique()->safeEmail,
+		'gender' => $gender,
+		'avatar' => 'default-'.$gender.'.jpg',
+		'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+		'remember_token' => str_random(10),
+	];
 });
