1e3d2cc101cd66afe46f6deb488ecc1351ca2254
Zach Preston <zpdrums@comcast.net>
2018-02-07 21:26:24 -0500

    fix favorites issue with timestamps removed

--- app/Favorite.php
+++ app/Favorite.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * The attributes that aren't mass assignable.
      *
      * @var array
