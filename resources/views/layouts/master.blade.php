a06404ed6a94e87ffa303ba2dba7ac2e5f5b11bc
Zach Preston <zpdrums@comcast.net>
2018-03-04 17:30:10 -0500

    enable cache busting in webpack file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     </head>
     <body>
         <div id="app">
@@ -20,7 +20,7 @@
             </section>
             @include('layouts.partials.footer')
         </div>
-        <script src="/js/app.js"></script>
+        <script src="{{ mix('/js/app.js') }}"></script>
         @yield('scripts')
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.sass', 'public/css')
-    .copy('node_modules/font-awesome/fonts', 'public/fonts');
+    .copy('node_modules/font-awesome/fonts', 'public/fonts')
+    .version();
