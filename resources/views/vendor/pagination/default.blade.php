f089103c80a828ff7a84b6b2c50529739ee7cb2c
Zach Preston <zpdrums@comcast.net>
2018-08-05 13:13:33 -0400

    update pagination view

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,38 +1,40 @@
 @if ($paginator->hasPages())
-    <nav class="pagination">
-        <!-- Previous Page Link -->
+    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+        {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="pagination-previous is-disabled">Previous</a>
+            <a class="pagination-previous" disabled>Previous</a>
         @else
             <a class="pagination-previous" href="{{ $paginator->previousPageUrl() }}">Previous</a>
         @endif
 
-        <!-- Next Page Link -->
+        {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}">Next page</a>
+            <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}">Next Page</a>
         @else
-            <a class="pagination-next is-disabled">Next page</a>
+            <a class="pagination-next" disabled>Next Page</a>
         @endif
 
+
+        {{-- Pagination Elements --}}
         <ul class="pagination-list">
-            <!-- Pagination Elements -->
             @foreach ($elements as $element)
-                <!-- "Three Dots" Separator -->
+                {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li><span class="pagination-ellipsis">{{ $element }}</span></li>
+                    <li><span class="pagination-ellipsis">&hellip;</span></li>
                 @endif
 
-                <!-- Array Of Links -->
+                {{-- Array Of Links --}}
                 @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li><a class="pagination-link is-current" href="#!">{{ $page }}</a></li>
-                        @else
-                            <li><a class="pagination-link" href="{{ $url }}">{{ $page }}</a></li>
-                        @endif
-                    @endforeach
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <li><a class="pagination-link is-current" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
+                            @else
+                                <li><a href="{{ $url }}" class="pagination-link" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
+                            @endif
+                        @endforeach
                 @endif
             @endforeach
         </ul>
+
     </nav>
 @endif
