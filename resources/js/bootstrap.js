af6aad3002cee1606fedcc5973cb8d410e811b9e
Zach Preston <zpdrums@comcast.net>
2019-12-07 11:58:52 -0500

    minor update to bootstrap js file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,5 +30,5 @@ window.flash = function (message, type='success') {
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
+//     forceTLS: true
 // });
