9da0eacb08b431622b314a60f1551ce1b1c4b21b
Zach Preston <zpdrums@comcast.net>
2020-10-30 20:40:11 -0400

    minor fix to profile index view

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 	{{ Form::open(['route' => ['profiles.index'], 'method' => 'GET']) }}
 		<div class="control has-icons-left">
-			{{ Form::search('q', null,['class' => 'input is-medium', 'placeholder' => 'Search...']) }}
+			{{ Form::search('q', null, ['class' => 'input is-medium', 'placeholder' => 'Search...']) }}
 			<span class="icon is-left">
 				<i class="fas fa-search"></i>
 			</span>
