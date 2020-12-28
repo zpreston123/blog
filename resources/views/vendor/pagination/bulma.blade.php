c724c4685a58bb966310de9e854349ae31a9f27e
Zach Preston <zpdrums@comcast.net>
2020-12-28 09:57:21 -0500

    update bulma pagination view

--- resources/views/vendor/pagination/bulma.blade.php
+++ resources/views/vendor/pagination/bulma.blade.php
@@ -1,36 +1,33 @@
 @if ($paginator->hasPages())
     <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-        @if ($paginator->onFirstPage())
-            <a class="pagination-previous" disabled>Previous</a>
-        @else
-            <a href="{{ $paginator->previousPageUrl() }}" class="pagination-previous">Previous</a>
-        @endif
-
-        @if ($paginator->hasMorePages())
-            <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}">Next</a>
-        @else
-            <a class="pagination-next" disabled>Next page</a>
-        @endif
 
         <ul class="pagination-list">
+
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li><span class="pagination-ellipsis">{{ $element }}</span></li>
+                    <span class="pagination-ellipsis">&hellip;</span>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li><a class="pagination-link is-current" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
+                            <li>
+                                <a class="pagination-link is-current" aria-label="Page {{ $page }}"
+                                   aria-current="page">{{ $page }}</a>
+                            </li>
                         @else
-                            <li><a href="{{ $url }}" class="pagination-link" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
+                            <li>
+                                <a href="{{ $url }}" class="pagination-link"
+                                   aria-label="Goto page {{ $page }}">{{ $page }}</a>
+                            </li>
                         @endif
                     @endforeach
                 @endif
             @endforeach
+
         </ul>
     </nav>
 @endif
