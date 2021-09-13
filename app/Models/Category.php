68f6df5144c93b7eb28cf01b063eea6d37f73312
Zach Preston <zpdrums@comcast.net>
2021-09-13 17:24:54 -0400

    minor update to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = ['name'];
 
