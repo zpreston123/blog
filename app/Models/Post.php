b1e8cbbb5652665e4e7bdb385f95b7c6a5536b78
Zach Preston <zpdrums@comcast.net>
2021-09-13 17:25:16 -0400

    minor update to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,7 +13,7 @@ class Post extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = ['title', 'body'];
 
