3f95e9be53c7c12e47bc2026f8747c2b314376ad
Zach Preston <zpdrums@comcast.net>
2022-07-09 19:43:25 -0400

    refactor imports in register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\{Hash, Validator};
 
 class RegisterController extends Controller
 {
