b9ec54e061a628917042b60e1a0ae1bad5992a0f
Zach Preston <zpdrums@comcast.net>
2022-07-09 21:02:53 -0400

    reorder imports in app service provider class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Http\View\Composers\{CategoryComposer, TagComposer};
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
