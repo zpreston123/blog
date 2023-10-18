181b48c5f0a904905f2e065fe6579c8133424ba7
Zach Preston <zpdrums@comcast.net>
2023-10-18 19:07:33 -0400

    rewrite login form syntax

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,68 @@
 <div class="box">
-    {{ Form::open(['route' => 'login']) }}
+    {{ html()->form('POST', route('login'))->open() }}
         <div class="field">
             <div class="control has-icons-left">
-                {{ Form::text('email', old('email'), ['class' => 'input' . ($errors->has('email') ? ' is-danger' : ''), 'placeholder' => 'Email']) }}
+                {{ html()
+                    ->text('email', old('email'))
+                    ->class('input' . ($errors->has('email') ? ' is-danger' : ''))
+                    ->placeholder('Email')
+                }}
                 <span class="icon is-small is-left">
                     <i class="fas fa-envelope"></i>
                 </span>
             </div>
-            {!! $errors->first('email', '<p class="help is-danger">:message</p>') !!}
+            {{ html()
+                ->p('email')
+                ->text($errors->first('email'))
+                ->class('help is-danger')
+            }}
         </div>
 
         <div class="field">
             <div class="control has-icons-left">
-                {{ Form::password('password', ['class' => 'input' . ($errors->has('password') ? ' is-danger' : ''), 'placeholder' => 'Password']) }}
+                {{ html()
+                    ->password('password')
+                    ->class('input' . ($errors->has('password') ? ' is-danger' : ''))
+                    ->placeholder('Password')
+                }}
                 <span class="icon is-small is-left">
                     <i class="fas fa-lock"></i>
                 </span>
             </div>
-            {!! $errors->first('password', '<p class="help is-danger">:message</p>') !!}
+            {{ html()
+                ->p('password')
+                ->text($errors->first('password'))
+                ->class('help is-danger')
+            }}
         </div>
 
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    {{ Form::checkbox('remember', old('remember'), old('remember') ? true : false) }}
+                    {{ html()->checkbox('remember', old('remember'), old('remember') ? true : false) }}
                     Remember me
                 </label>
             </div>
         </div>
 
         <div class="control">
-            {{ Form::button('
-                <span class="icon">
-                    <i class="fas fa-sign-in-alt"></i>
-                </span>
-                <span>Log in</span>',
-                [
-                    'type' => 'submit',
-                    'class' => 'button is-info is-fullwidth'
-                ])
+            {{ html()
+                ->button('
+                    <span class="icon">
+                        <i class="fas fa-sign-in-alt"></i>
+                    </span>
+                    <span>Log in</span>',
+                    'submit'
+                )
+                ->class('button is-info is-fullwidth')
             }}
         </div>
 
         <div class="control has-text-centered">
-            <a class="button is-small is-text" href="{{ route('password.request') }}">
-                Forgot Your Password?
-            </a>
+            {{ html()
+                ->a(route('password.request'), 'Forgot Your Password?')
+                ->class('button is-small is-text')
+            }}
         </div>
-    {{ Form::close() }}
+    {{ html()->form()->close() }}
 </div>
