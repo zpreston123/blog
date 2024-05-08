a694364ea8bbcd4b903c8c39787a930a54468d9b
Zach Preston <zpdrums@comcast.net>
2024-05-07 21:27:22 -0400

    remove docblocks from model factories

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -7,18 +7,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Comment::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
 		return [
