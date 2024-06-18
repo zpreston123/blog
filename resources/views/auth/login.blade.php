8aa90487d1127f22d9b6cac62c33a8e31d918cb3
Zach Preston <zpdrums@comcast.net>
2024-06-17 21:06:23 -0400

    convert login form to plain html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,49 @@
-<div class="box">
-    {{ html()->form('POST', route('login'))->open() }}
-        <div class="field">
-            <div class="control has-icons-left">
-                {{ html()
-                    ->text('email', old('email'))
-                    ->class('input' . ($errors->has('email') ? ' is-danger' : ''))
-                    ->placeholder('Email')
-                }}
-                <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                </span>
-            </div>
-            {{ html()
-                ->p('email')
-                ->text($errors->first('email'))
-                ->class('help is-danger')
-            }}
-        </div>
+<form class="box" action="{{ route('login') }}" method="POST">
+    @csrf
 
-        <div class="field">
-            <div class="control has-icons-left">
-                {{ html()
-                    ->password('password')
-                    ->class('input' . ($errors->has('password') ? ' is-danger' : ''))
-                    ->placeholder('Password')
-                }}
-                <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                </span>
-            </div>
-            {{ html()
-                ->p('password')
-                ->text($errors->first('password'))
-                ->class('help is-danger')
-            }}
+    <div class="field">
+        <div class="control has-icons-left">
+            <input type="text" name="email" class="input @error('email') is-danger @enderror" placeholder="Email" value="{{ old('email') }}">
+            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+            </span>
         </div>
+        @error('email')
+            <p class="help is-danger">{{ $message }}</p>
+        @enderror
+    </div>
 
-        <div class="field">
-            <div class="control">
-                <label class="checkbox">
-                    {{ html()->checkbox('remember', old('remember'), old('remember') ? true : false) }}
-                    Remember me
-                </label>
-            </div>
+    <div class="field">
+        <div class="control has-icons-left">
+            <input type="password" name="password" class="input @error('password') is-danger @enderror" placeholder="Password">
+            <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+            </span>
         </div>
+        @error('password')
+            <p class="help is-danger">{{ $message }}</p>
+        @enderror
+    </div>
 
+    <div class="field">
         <div class="control">
-            {{ html()
-                ->button('
-                    <span class="icon">
-                        <i class="fas fa-sign-in-alt"></i>
-                    </span>
-                    <span>Log in</span>',
-                    'submit'
-                )
-                ->class('button is-info is-fullwidth')
-            }}
+            <label class="checkbox">
+                <input type="checkbox" name="remember" value="remember">
+                Remember me
+            </label>
         </div>
+    </div>
 
-        <div class="control has-text-centered">
-            {{ html()
-                ->a(route('password.request'), 'Forgot Your Password?')
-                ->class('button is-small is-text')
-            }}
-        </div>
-    {{ html()->form()->close() }}
-</div>
+    <div class="control">
+        <button type="submit" class="button is-info is-fullwidth">
+            <span class="icon">
+                <i class="fas fa-sign-in-alt"></i>
+            </span>
+            <span>Log in</span>
+        </button>
+    </div>
+
+    <div class="control has-text-centered">
+        <a href="{{ route('password.request') }}" class="button is-small is-text">Forgot Your Password</a>
+    </div>
+</form>
