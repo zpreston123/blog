f04f43f880409ba9c3963e203f385a420fc827d8
Zach Preston <zpdrums@comcast.net>
2024-09-02 14:14:58 -0400

    convert post search form to plain html

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,8 +1,8 @@
-{{ Form::open(['route' => 'posts.search', 'style' => 'padding-bottom: 40px;', 'method' => 'GET']) }}
+<form action="{{ route('posts.search') }}" style="padding-bottom: 40px;" method="GET">
 	<div class="control has-icons-left">
-		{{ Form::search('q', null,['class' => 'input is-medium', 'placeholder' => 'Search...']) }}
+		<input name="q" class="input is-medium" placeholder="Search...">
 	    <span class="icon is-left">
 			<i class="fas fa-search"></i>
 	    </span>
 	</div>
-{{ Form::close() }}
+</form>
