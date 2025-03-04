fa4833b4e52c0b99a6f8a0a7b1891f55bad6bfb0
Zach Preston <zpdrums@comcast.net>
2025-03-03 19:32:46 -0500

    refactor TagFactory class

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,17 +2,14 @@
 
 namespace Database\Factories;
 
-use App\Models\Tag;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TagFactory extends Factory
 {
-    protected $model = Tag::class;
-
-    public function definition()
+    public function definition(): array
     {
       return [
-        'name' => $this->faker->unique()->word
+        'name' => fake()->unique()->word
       ];
     }
 }
