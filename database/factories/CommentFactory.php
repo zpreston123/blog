5044fdba4ec80eda266505afbe6adad9514aee17
Zach Preston <zpdrums@comcast.net>
2025-03-03 19:32:19 -0500

    refactor CommentFactory class

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,17 +2,15 @@
 
 namespace Database\Factories;
 
-use App\Models\{Comment, Post, User};
+use App\Models\{Post, User};
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
 {
-    protected $model = Comment::class;
-
-    public function definition()
+    public function definition(): array
     {
         return [
-            'body' => $this->faker->sentence,
+            'body' => fake()->sentence,
             'user_id' => User::all()->random()->id,
             'post_id' => Post::all()->random()->id
         ];
